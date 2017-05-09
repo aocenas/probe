8f44d9818482c8f2633d8718331fdea7da3a62cf
Andrej Ocenas <mr.ocenas@gmail.com>
2017-05-10 01:01:16 +0200

    Transition

--- app/src/components/Flame.jsx
+++ app/src/components/Flame.jsx
@@ -1,3 +1,4 @@
+/* global document */
 const React = require('react');
 const PT = require('prop-types');
 const d3 = require('d3');
@@ -29,91 +30,60 @@ class Flame extends React.Component {
         const h = height * 20;
         let scaleX = d3.scaleLinear().range([0, w]);
         let scaleY = d3.scaleLinear().range([0, h]);
-        const scaleColor = d3.scaleLinear().range([35, 15]);
         let selected = null;
+        const trans = d3.transition().duration(250).ease(d3.easeCubicInOut);
 
         let svg = d3.select('svg.flame').attr('width', w).attr('height', h);
 
-        svg = svg.selectAll('g').data(descendants);
-        svg.exit().remove();
+        const colorSelected = d3.color(`hsl(100,100%,55.3%)`);
+        const colorDefault = d3.color(`hsl(40,100%,55.3%)`);
 
-        let g = svg
-            .enter()
-            .filter(d => {
-                return scaleX(d.x1) - scaleX(d.x0) > 0.3;
-            })
-            .append('svg:g')
-            .attr('transform', d => {
-                return 'translate(' + scaleX(d.x0) + ',' + scaleY(d.y0) + ')';
-            })
-            .on('click', d => {
-                selected = d;
-                scaleX = d3.scaleLinear().domain([d.x0, d.x1]).range([0, w]);
-                rescale();
+        const update = () => {
+            const filtered = descendants.filter(d => {
+                // filter data too small to show or not visible
+                return (
+                    scaleX(d.x1) - scaleX(d.x0) > 0.3
+                );
             });
+            let g = svg.selectAll('g').data(filtered, d => d.data.start);
 
-        g
-            .filter(d => {
-                return scaleX(d.x0) >= 0 && scaleX(d.x1) <= w;
-            })
-            .append('svg:rect')
-            .attr('stroke', 'white')
-            .attr('width', function(d) {
-                return Math.min(scaleX(d.x1) - scaleX(d.x0), w);
-            })
-            .attr('height', function(d) {
-                return scaleY(d.y1 - d.y0);
-            })
-            .attr('fill', function(d) {
-                if (d === selected) {
-                    return d3.color(`hsl(100,100%,55.3%)`);
-                } else {
-                    return d3.color(
-                        `hsl(${scaleColor(d.data.self / d.data.total)},100%,55.3%)`
-                    );
-                }
-            });
+            g.exit().remove();
 
-        g
-            .filter(d => {
-                return scaleX(d.x1) - scaleX(d.x0) > 2;
-            })
-            .attr('stroke-width', 2);
+            let enterG = g.enter().append('svg:g');
 
-        g
-            .filter(d => {
-                return scaleX(d.x1) - scaleX(d.x0) < 2;
-            })
-            .attr('stroke-width', 1);
+            enterG.append('svg:rect').attr('stroke', 'white');
 
-        g
-            .filter(d => {
-                return scaleX(d.x1) - scaleX(d.x0) > 15;
-            })
-            .append('foreignObject')
-            .attr('width', function(d) {
-                return scaleX(d.x1) - scaleX(d.x0);
-            })
-            .attr('height', function(d) {
-                return scaleY(d.y1 - d.y0);
-            })
-            .append('xhtml:div')
-            .attr('class', 'flame-label')
-            .text(function(d) {
-                return d.data.func || 'program';
-            });
+            let updateEnter = enterG.merge(g);
 
-        const rescale = () => {
-            let transitionDuration = 250;
+            updateEnter
+                .filter(d => {
+                    return scaleX(d.x1) - scaleX(d.x0) < 15;
+                })
+                .select('foreignObject')
+                .remove();
 
-            const g = d3
-                .select('svg.flame')
-                .selectAll('g')
-                .transition()
-                .duration(transitionDuration)
-                .ease(d3.easeCubicInOut)
+            updateEnter
+                .filter(d => {
+                    return scaleX(d.x1) - scaleX(d.x0) >= 15;
+                })
+                .select(function() {
+                    if (this.childNodes.length === 1) {
+                        let foreignObject = document.createElementNS(
+                            'http://www.w3.org/2000/svg',
+                            'foreignObject'
+                        );
+                        foreignObject.appendChild(
+                            document.createElement('div')
+                        );
+                        return this.appendChild(foreignObject);
+                    }
+                    return null;
+                });
+
+            g
+                .transition(trans)
                 .attr('transform', d => {
-                    if (selected.ancestors().includes(d)) {
+                    if (selected && selected.ancestors().includes(d)) {
                         return 'translate(0, ' + scaleY(d.y0) + ')';
                     } else {
                         return (
@@ -126,22 +96,89 @@ class Flame extends React.Component {
                     }
                 });
 
-            g.select('rect').attr('width', function(d) {
-                if (selected.ancestors().includes(d)) {
-                    return w;
-                } else {
-                    return scaleX(d.x1) - scaleX(d.x0);
-                }
-            });
+            g
+                .select('rect')
+                .transition(trans)
+                .attr('width', function(d) {
+                    if (selected && selected.ancestors().includes(d)) {
+                        return w;
+                    } else {
+                        return scaleX(d.x1) - scaleX(d.x0);
+                    }
+                });
 
-            g.select('foreignObject').attr('width', function(d) {
-                if (selected.ancestors().includes(d)) {
-                    return w;
-                } else {
-                    return scaleX(d.x1) - scaleX(d.x0);
-                }
-            });
+            enterG
+                .attr('transform', d => {
+                    if (selected && selected.ancestors().includes(d)) {
+                        return 'translate(0, ' + scaleY(d.y0) + ')';
+                    } else {
+                        return (
+                            'translate(' +
+                            scaleX(d.x0) +
+                            ',' +
+                            scaleY(d.y0) +
+                            ')'
+                        );
+                    }
+                });
+
+            enterG
+                .select('rect')
+                .attr('width', function(d) {
+                    if (selected && selected.ancestors().includes(d)) {
+                        return w;
+                    } else {
+                        return scaleX(d.x1) - scaleX(d.x0);
+                    }
+                });
+
+            updateEnter
+                .on('click', d => {
+                    selected = d;
+                    scaleX = d3
+                        .scaleLinear()
+                        .domain([d.x0, d.x1])
+                        .range([0, w]);
+                    update();
+                })
+                .select('rect')
+                .attr('height', function(d) {
+                    return scaleY(d.y1 - d.y0);
+                })
+                .attr('fill', function(d) {
+                    if (d === selected) {
+                        return colorSelected;
+                    } else {
+                        return colorDefault;
+                    }
+                })
+                .attr('stroke-width', d => {
+                    return scaleX(d.x1) - scaleX(d.x0) > 2 ? 2 : 1;
+                });
+
+            updateEnter
+                .select('foreignObject')
+                // .transition(trans)
+                .attr('width', function(d) {
+                    if (selected && selected.ancestors().includes(d)) {
+                        return w;
+                    } else {
+                        return scaleX(d.x1) - scaleX(d.x0);
+                    }
+                })
+                .attr('height', function(d) {
+                    return scaleY(d.y1 - d.y0);
+                });
+
+            updateEnter
+                .select('foreignObject')
+                .select('div')
+                .attr('class', 'flame-label')
+                .text(function(d) {
+                    return d.data.func || 'program';
+                });
         };
+        update();
     }
 
     componentDidUpdate() {}
